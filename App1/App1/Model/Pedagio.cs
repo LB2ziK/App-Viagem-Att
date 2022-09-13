f5e0a0263ea9ee6f0d46edd9cd070aa47610caf2
LB2ziK <lh.rissilb2@gmail.com>
2022-09-13 12:16:01 -0300

    Adicionar arquivos de projeto.

---
+++ App1/App1/Model/Pedagio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace App1.Model
+{
+    public class Pedagio
+    {
+        
+        public int Id { get; set; }
+        public int Id_Viagem { get; set; }
+        public string Localizacao { get; set; }
+        public double Valor { get; set; }
+    }
+}
